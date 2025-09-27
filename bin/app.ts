c774380667fe4d4d25168d0c9cf8c4404541b02e
Nasser Rahal <nasser.rahal@gmail.com>
2025-09-27 15:44:06 +0400

    refactor to remove hard coded websites

--- bin/app.ts
+++ bin/app.ts
@@ -1,6 +1,7 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { StaticSiteWithCFAcmStack } from "../lib/static-site-cloudfront";
+import { DOMAIN_NAME } from "../lib/domain-config";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -8,10 +9,11 @@ const app = new cdk.App();
 
 const account = process.env.CDK_ACCOUNT!;
 const region = process.env.CDK_REGION!;
+const stackName = process.env.CDK_STACK_NAME || "StaticSiteStack";
 
-new StaticSiteWithCFAcmStack(app, "VoltgraphSiteStack", {
+new StaticSiteWithCFAcmStack(app, stackName, {
   env: { account, region },
-  domainName: "voltgraph.com",
+  domainName: DOMAIN_NAME,
   includeWww: true,
   priceClass: "PriceClass_100",
   removalPolicy: "RETAIN",
---
+++ lib/domain-config.ts
@@ -0,0 +1,2 @@
+// This file exports the domain name for the static site deployment
+export const DOMAIN_NAME = "voltgraph.com";
